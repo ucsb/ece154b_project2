556826091699ac6430c046a67400269a3a3456c7
Marko Ristic <markoristic@ucsb.edu>
2022-01-17 15:19:28 -0800

    multiplyer edited to work w/ sign multiplication

--- other.v
+++ other.v
@@ -145,44 +145,77 @@ module multserial(input wire CLK,
                   input wire [31:0] SRCB,
                   output wire [63:0] PROD,
                   output wire PRODV);
-    reg [1:0] state = 3;
-    reg [63:0] P, T;
-    reg [31:0] temp_b;
-    assign 
+    reg [2:0] state <= 3; //3 will be our default state of no change
+    reg msgn; //used to store signed or unsigned, as we need it for the last bit of calculation
+    reg [63:0] P, T; //product and tempororary registers
+    reg [31:0] temp_b; //used to hold B, we will shift right by 1 each multiplication cycle
     always @(posedge CLK) begin
-        if(MST) begin
-            state = 0;
-        end
         case (state)
-            0: begin
-                count <= 0;
+            0: begin //MST was received previous cycle, load registers and go to multiply state (1)
                 P <= 0;
+                PRODV <= 1'b0;
                 temp_b <= b;
                 T[31:0] <= a;
-                T[63:32] <= 32'b0;
+                T[63:32] <= 32'b0; //each iteration we shift to the right, so that each row of multiplication is calculated correctly
                 state <=1;
             end
-            1: begin
-                if(count == 33'b11111111111111111111111111111111) begin
-                    state <= 2;
+            1: begin //calculating product
+                if(RST) begin //go to reset state
+                    state <= 4;
                 end else begin
-                    if(temp_b[0] == 1'b1)
-                        P <= P + T;
-                    else
+                    if(temp_b[0] == 1'b1) begin
+                        if(temp_b == 1 && msgn) begin //last bit of B is sign bit so must subtract to ensure correct calculation
+                            P <= P - T;
+                            state <= 2; 
+                        end else begin //either unsigned or not last bit so add to product
+                            P <= P + T;
+                        end
+                    end else begin //value of temp_b[0] is 0 so no multiplication for this "level"
                         P <= P;
-                    temp_b <= temp_b>>1;
-                    T <= T<<1;
-                    count <= count + 1;
-                    state <= s1;
+                    end
+                    if(temp_b == 0) begin //finished shifting through B, i.e. our product is finished being calculated
+                        state <= 2;
+                    end else begin //not done multiplying
+                        temp_b <= temp_b>>1; //shift right by 1 to use next b[0] in next multiplicative cycle 
+                        T <= T<<1; //shift left by 1 so that when adding to P, we accout for our b[0] shift
+                        state <= 1;
+                    end
                 end
             end
-            2: begin
+            2: begin //output state
                 PROD <= P;
                 PRODV <= 1'b1;
-                state <= 3;
+                if(RST) begin //reset signal
+                    state <= 4;
+                end else if (MST) begin //next cycle begin multiply (load regs)
+                    state <= 1; 
+                end else begin //default state
+                    state <= 3;
+                end
             end
-            3:
-            default: ;
+            3: begin //default, do nothing state
+                if(RST) begin  //reset state sends us to 4
+                    state <= 4;
+                end else if(MST) begin //next cycle begin multiply
+                    state <= 1;
+                end else begin 
+                    state <= 3;
+                end
+            end
+            4: begin //reset state
+                temp_b <= 0;
+                msgn <= 0;
+                P <= 0;
+                T <= 0;
+                PRODV <= 0;
+                if(MST) begin
+                    state <= 0;
+                end
+                else begin
+                    state <= 4;
+                end
+            end
+            default: 
         endcase
     end
 endmodule
